f738c9001db3ff1822407131e51b5b365b5f428c
pauditicha <pauditicha@gmail.com>
2023-06-16 12:50:54 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 if success
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m <= 57; m++)
+	{
+		putchar(m);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
