bb7dd4462ea23f94bb7be595aa4177e1490ecedd
pauditicha <pauditicha@gmail.com>
2023-06-16 12:36:17 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the program
+ *
+ * Return 0 if success
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		printf("%i", k);
+	}
+
+	printf("\n");
+
+	return (0);
+}
