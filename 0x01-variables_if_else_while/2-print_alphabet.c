fc2fa227e98985d846b550b62957a3ef39ee428e
pauditicha <pauditicha@gmail.com>
2023-06-16 12:10:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: Always 0 if success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = "a"; letter <= "z"; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
