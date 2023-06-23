b59cd86cfeb0a1aa4927cb32003037745bb030d2
pauditicha <pauditicha@gmail.com>
2023-06-23 10:18:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int j;
+
+	j = 0;
+	while (j < 10)
+	{
+		_putchar(j + '0');
+		i++;
+	}
+	_putchar ('\n');
+}
