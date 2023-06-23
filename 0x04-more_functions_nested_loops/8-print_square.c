0b62494e063b67474610e39bf2dc1338b240feca
pauditicha <pauditicha@gmail.com>
2023-06-23 11:21:49 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ *
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar(35);
+		}
+		if (a < (size - 1))
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
