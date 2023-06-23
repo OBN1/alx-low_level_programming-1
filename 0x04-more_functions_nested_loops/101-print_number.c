c2298c97fc050f3c18506f0a3d59eacfd8494788
pauditicha <pauditicha@gmail.com>
2023-06-23 12:10:08 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: input integer parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+	unsigned int j = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		j = -j;
+	}
+	if (j / 10)
+	{
+		print_number(j / 10);
+	}
+	_putchar(j % 10 + '0');
+}
