abb4d97ca3cefbd0d8d16c569c8504570a53bede
pauditicha <pauditicha@gmail.com>
2023-06-27 00:22:13 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Resets the value of n to 98
+ * @n: A pointer to an integer data type that will be updated.
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
