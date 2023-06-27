7946832fc928f4521bfb7f55e2d6645237f50be3
pauditicha <pauditicha@gmail.com>
2023-06-27 00:31:17 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Swaps two integers
+ * @a: First integer to be entered
+ * @b: Second integer to be entered
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
