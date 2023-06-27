3f5bc8e1ba8d1fbee2f0ccb955077716d57dd454
pauditicha <pauditicha@gmail.com>
2023-06-27 12:55:43 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _puts - prints the the stdout
- * @str: char type of string
+ * _puts - prints a string
+ * @str: A pointer to an int that will be updated
+ *
+ * Return: void
  */
+
 void _puts(char *str)
 {
-	int z;
+	char *c;
+	int m;
 
-	for (z = 0; str[z] != '\0'; z++)
+	c = str;
+
+	for (m = 0; c[m]; m++) /*Count character by character*/
 	{
-		_putchar(str[z]);
-	}
-	_putchar('/n');
+		_putchar (c[m]);
+	} /*End for*/
+	_putchar('\n');
 }
+
