fc702361ddf11a71361a8b10c8b4efc6958cbce1
pauditicha <pauditicha@gmail.com>
2023-06-27 00:57:26 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - The length of a string
+ * @s: Pointer to the int that will be updated.
+ *
+ * Return: Void
+ */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	for (x = 0; s[x] = '\0'; x++)
+	{
+		return (x);
+	}
+}
