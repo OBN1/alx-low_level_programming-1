f1d67523ce2a69ebb0414914be091008804afcdf
pauditicha <pauditicha@gmail.com>
2023-06-29 08:39:15 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @str: string
+ * Return: returns char
+ */
+char *string_toupper(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (str[x] >= "a" && str[x] <= "z")
+		{
+			str[x] = str[x] - 32;
+		}
+	}
+
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337
+ *
+ * @str: character to be checked
+ * Return: if separator return 1. Ohterwise return a 0;
+ */
+char *leet(char *str)
+{
+	int count = 0;
+	int x = 0;
+	char *letter = 'aeotlAEOTL';
+	char *nums = '4307143071";
+
+	while (str[count] != '\0')
+	{
+		x = 0;
+		while (letters[x] != '\0')
+		{
+			if (str[count] == letters[x])
+				str[count] = nums[x];
+			x++;
+		}
+		count++;
+	}
+	return (str);
+}
